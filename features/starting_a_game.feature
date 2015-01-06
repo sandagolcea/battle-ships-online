77279d3e0cf510547fd0b1951fb43dee70af1a63
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-06 17:54:58 +0000

    Fixes cucumber tests

---
+++ features/starting_a_game.feature
@@ -0,0 +1,9 @@
+Feature: Starting the game
+  In order to play battleships
+  As a nostalgic player
+  I want to start a new game
+
+  Scenario: Registering
+    Given I am on the homepage
+    When I follow "New Game"
+    Then I should see "What's your name?"
--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,10 +1,16 @@
 require 'sinatra/base'
 
 class BattleShips < Sinatra::Base
+set :views, Proc.new { File.join(root, "..", "views") }
+
   get '/' do
-    'Hello BattleShips!'
+    # 'Hello BattleShips!'
+    erb :index
   end
 
+  get '/newgame' do
+    "What's your name?"
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
